d15ed78e86d4542917eaa04efff19d7d09e4029d
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-12-02 16:39:01 +0900

    update: 게임 종료 후 기록 저장 로직 추가
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>

--- src/utils/notification/gameEnd.notification.js
+++ src/utils/notification/gameEnd.notification.js
@@ -3,8 +3,9 @@ import { Packets } from '../../init/loadProtos.js';
 import { createResponse } from '../response/createResponse.js';
 import { removeGameSession } from '../../sessions/game.session.js';
 import { roomManager } from '../../classes/manager/room.manager.js';
+import { saveRecord } from '../../db/record/record.db.js';
 
-export const gameEndNotification = (room) => {
+export const gameEndNotification = async (room) => {
   const survivor = [];
   room.users.forEach((user) => {
     if (user.characterData.hp > 0) {
@@ -20,10 +21,11 @@ export const gameEndNotification = (room) => {
 
   let responsePayload;
 
+  let winners = null;
   if (!isTarget && !isBodyguard && !isHitman) {
     //싸이코패스 승리
     console.log('싸이코패스 승리');
-    const winners = room.users.filter(
+    winners = room.users.filter(
       (user) => user.characterData.roleType === Packets.RoleType.PSYCHOPATH,
     );
     const winnerIds = winners.map((user) => user.id);
@@ -36,7 +38,7 @@ export const gameEndNotification = (room) => {
   } else if (!isHitman && !isPsychopath) {
     //타겟 & 보디가드 승리
     console.log('타겟 & 보디가드 승리');
-    const winners = room.users.filter(
+    winners = room.users.filter(
       (user) =>
         user.characterData.roleType === Packets.RoleType.TARGET ||
         user.characterData.roleType === Packets.RoleType.BODYGUARD,
@@ -51,9 +53,7 @@ export const gameEndNotification = (room) => {
   } else if (!isTarget) {
     //히트맨 승리
     console.log('히트맨 승리');
-    const winners = room.users.filter(
-      (user) => user.characterData.roleType === Packets.RoleType.HITMAN,
-    );
+    winners = room.users.filter((user) => user.characterData.roleType === Packets.RoleType.HITMAN);
     const winnerIds = winners.map((user) => user.id);
     responsePayload = {
       gameEndNotification: {
@@ -62,8 +62,19 @@ export const gameEndNotification = (room) => {
       },
     };
   }
+
   console.log('인터벌 작동 - ', responsePayload);
   if (responsePayload) {
+    // 기록 저장
+    const losers = room.users.filter((user) => !winners.some((winner) => winner.id === user.id));
+    winners.forEach(async (winner) => {
+      await saveRecord(winner.id, true, winner.characterData.roleType);
+    });
+
+    losers.forEach(async (loser) => {
+      await saveRecord(loser.id, false, loser.characterData.roleType);
+    });
+
     // intervalManager.clearAll();
     roomManager.deleteRoom(room.id);
     room.users.forEach((user) => {
