e0a1f5ad41c1d685642a5383f42fbb8a7f807c6a
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-12-02 12:21:35 +0900

    update: DB 변경에 따라서, 필요할 것이라 예상되는 transaction 함수 작성. 게임 기록 작성 시 사용될 예정
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/db/database.js
+++ src/db/database.js
@@ -34,4 +34,38 @@ const pools = {
   USER_DB: createPool(database.USER_DB),
 };
 
+const getConnection = async () => {
+  try {
+    const conn = await pools.USER_DB.getConnection();
+    return conn;
+  } catch (err) {
+    if (conn) {
+      conn.rollback();
+    }
+
+    console.error(err);
+    return null;
+  } finally {
+    if (conn) {
+      await conn.release();
+    }
+  }
+};
+
+const transaction = async (queries) => {
+  let conn = null;
+  try {
+    conn = await getConnection();
+    await conn.beginTransaction();
+
+    const result = await queries();
+
+    await conn.commit();
+    return result;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
 export default pools;
