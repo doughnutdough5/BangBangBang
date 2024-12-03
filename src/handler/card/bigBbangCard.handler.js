226048a3fe46912aff32142381ea03cf23ddf5ba
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-12-03 16:21:03 +0900

    fix: 주석 제거, TODO 추가

--- src/handler/card/bigBbangCard.handler.js
+++ src/handler/card/bigBbangCard.handler.js
@@ -5,11 +5,10 @@ import {
 } from '../../constants/stateType.js';
 import userUpdateNotification from '../../utils/notification/userUpdate.notification.js';
 
+// TODO: 피 2씩 다는 거 수정해야함.
 export const bigBbangCardHandler = (cardUsingUser, targetUser, currentGame, useCardType) => {
   currentGame.users.forEach((user) => {
     if (cardUsingUser.id !== user.id && 0 < user.characterData.hp) {
-      //나를 제외한 살아있는 모두에게 피해 1
-      //대상자 상태 변경
       cardUsingUser.setCharacterState(getStateBigBbangShooter(user.id));
       user.setCharacterState(getStateBigBbangTarget(cardUsingUser.id));
       currentGame.events.scheduleEvent(user.id, 'finishShieldWaitOnBigBbang', 5000, {
