0cdf46886c096796696b04955eb3f0643e4ad67e
bigsmile1460 <wns4711@naver.com>
2024-12-06 18:13:37 +0900

    fix: 상대방의 모든 카드가 존재하지 않는 경우 처리 추가
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: doughnutdough5 <doughnutdough5@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>

--- src/handler/card/normal/absorbCard.handler.js
+++ src/handler/card/normal/absorbCard.handler.js
@@ -1,6 +1,8 @@
 import { getStateAbsorbing, getStateAbsorbTarget } from '../../../constants/stateType.js';
 
 export const absorbCardHandler = (cardUsingUser, targetUser, currentGame, useCardType) => {
-  cardUsingUser.setCharacterState(getStateAbsorbing(targetUser.id));
-  targetUser.setCharacterState(getStateAbsorbTarget(cardUsingUser.id));
+  if(targetUser.characterData.weapon !== 0 || targetUser.characterData.equips.length !== 0 || targetUser.characterData.debuffs.length !== 0 || targetUser.getHandCardsCount() > 0){
+    cardUsingUser.setCharacterState(getStateAbsorbing(targetUser.id));
+    targetUser.setCharacterState(getStateAbsorbTarget(cardUsingUser.id));
+  }
 };
--- src/handler/card/normal/hallucinationCard.handler.js
+++ src/handler/card/normal/hallucinationCard.handler.js
@@ -4,6 +4,8 @@ import {
 } from '../../../constants/stateType.js';
 
 export const hallucinationCardHandler = (cardUsingUser, targetUser, currentGame, useCardType) => {
-  cardUsingUser.setCharacterState(getStateHallucinating(targetUser.id));
-  targetUser.setCharacterState(getStateHallucinationTarget(cardUsingUser.id));
+  if(targetUser.characterData.weapon !== 0 || targetUser.characterData.equips.length !== 0 || targetUser.characterData.debuffs.length !== 0 || targetUser.getHandCardsCount() > 0){
+    cardUsingUser.setCharacterState(getStateHallucinating(targetUser.id));
+    targetUser.setCharacterState(getStateHallucinationTarget(cardUsingUser.id));
+  }
 };
